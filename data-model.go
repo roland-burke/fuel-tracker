56c880ee768479cee9e26b943db47b3522bb875e
roland-burke <roland.burke@gmx.de>
2021-09-23 13:08:50 +0200

    added distance

--- data-model.go
+++ data-model.go
@@ -1,6 +1,8 @@
 package main
 
-import "time"
+import (
+	"time"
+)
 
 type Configuration struct {
 	AuthToken string `json:"authToken"`
--- db-setup.sql
+++ db-setup.sql
@@ -12,10 +12,10 @@ CREATE TABLE IF NOT EXISTS refuel (
     id                      SERIAL NOT NULL PRIMARY key,
     name                    varchar(50) NOT NULL,
     date_time               timestamp NOT NULL,
-    price_per_liter_euro    float8 constraint price_euro_not_negative_or_zero CHECK (price_per_liter_euro IS NULL OR price_per_liter_euro > 0),
+    price_per_liter_euro    float8 NOT NULL constraint price_euro_not_negative CHECK (price_per_liter_euro >= 0),
     total_liter             float8 NOT NULL constraint total_liter_not_negative_or_zero CHECK (total_liter > 0),
-    price_per_liter         float8 constraint price_not_negative_or_zero CHECK (price_per_liter IS NULL OR price_per_liter > 0),
-    currency                varchar(12),
+    price_per_liter         float8 NOT NULL constraint price_not_negative CHECK (price_per_liter >= 0),
+    currency                varchar(12) NOT NULL,
     distance                float8 NOT NULL constraint distance_not_negative_or_zero CHECK (distance > 0),
     updated_at              TIMESTAMPTZ NOT NULL DEFAULT NOW()
 );
@@ -29,3 +29,5 @@ INSERT INTO public.refuel ("name",date_time,price_per_liter_euro,total_liter,pri
 	 ('LPG Kreuzlingen','2021-09-04 13:10:25',1.439,42.0,1.488,'chf', 560.6);
 INSERT INTO public.refuel ("name",date_time,price_per_liter_euro,total_liter,price_per_liter,currency,distance) VALUES
 	 ('LPG Kreuzlingen Test','2021-09-15 16:12:11',1.368,34.0,1.420,'chf', 420.8);
+INSERT INTO public.refuel ("name",date_time,price_per_liter_euro,total_liter,price_per_liter,currency,distance) VALUES
+	 ('Shell Konstanz','2021-09-15 16:12:11',1.579,23.6,0,'', 356.1);
