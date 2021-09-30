f31382d21e3b9b6ed06625520b8eb98d5a1553c4
roland-burke <roland.burke@gmx.de>
2021-09-30 15:01:11 +0200

    splited db init files

---
+++ database/db-init-dev-02.sql
@@ -0,0 +1,25 @@
+INSERT INTO users ("username",pass_key) VALUES
+	 ('john', 'john');
+
+INSERT INTO users ("username",pass_key) VALUES
+	 ('mary', 'mary');
+
+
+INSERT INTO refuel (users_id,"description",date_time,price_per_liter_euro,total_liter,price_per_liter,"currency","mileage",license_plate) VALUES
+	 (1,'LPG Kreuzlingen','2021-09-04 13:10:25',1.439,42.0,1.488,'chf',560.6,'KNKN9889');
+
+INSERT INTO refuel (users_id,"description",date_time,price_per_liter_euro,total_liter,price_per_liter,"currency","mileage",license_plate) VALUES
+	 (1,'LPG Kreuzlingen Test','2021-06-14 16:12:11',1.368,34.0,1.420,'chf',420.8,'KNQW123');
+
+INSERT INTO refuel (users_id,"description",date_time,price_per_liter_euro,total_liter,price_per_liter,"currency","mileage",license_plate) VALUES
+	 (1,'Shell Konstanz','2020-09-15 16:12:11',1.579,23.6,0,'',356.1,'KNGH34');
+
+
+INSERT INTO refuel (users_id,"description",date_time,price_per_liter_euro,total_liter,price_per_liter,"currency","mileage",license_plate) VALUES
+	 (2,'Shell Konstanz','2020-08-13 16:12:11',1.579,23.6,0,'',356.1,'KNGH34');
+
+INSERT INTO refuel (users_id,"description",date_time,price_per_liter_euro,total_liter,price_per_liter,"currency","mileage",license_plate) VALUES
+	 (2,'LPG Kreuzlingen','2021-09-04 13:10:25',1.439,42.0,1.488,'chf',560.6,'KNKN9889');
+
+INSERT INTO refuel (users_id,"description",date_time,price_per_liter_euro,total_liter,price_per_liter,"currency","mileage",license_plate) VALUES
+	 (2,'LPG Kreuzlingen Test','2021-09-15 16:12:11',1.368,34.0,1.420,'chf',420.8,'KNQW123');
--- database/db-setup-01.sql
+++ database/db-setup-01.sql
@@ -37,18 +37,3 @@ CREATE TRIGGER set_timestamp_users
 BEFORE UPDATE ON users
 FOR EACH ROW
 EXECUTE FUNCTION trigger_set_timestamp();
-
-INSERT INTO users ("username",pass_key) VALUES
-	 ('admin', 'adminuser1');
-
-INSERT INTO users ("username",pass_key) VALUES
-	 ('john', 'johnpassword');
-
-INSERT INTO refuel (users_id,"description",date_time,price_per_liter_euro,total_liter,price_per_liter,"currency","mileage",license_plate) VALUES
-	 (1,'LPG Kreuzlingen','2021-09-04 13:10:25',1.439,42.0,1.488,'chf',560.6,'KNKN9889');
-
-INSERT INTO refuel (users_id,"description",date_time,price_per_liter_euro,total_liter,price_per_liter,"currency","mileage",license_plate) VALUES
-	 (1,'LPG Kreuzlingen Test','2021-09-15 16:12:11',1.368,34.0,1.420,'chf',420.8,'KNQW123');
-
-INSERT INTO refuel (users_id,"description",date_time,price_per_liter_euro,total_liter,price_per_liter,"currency","mileage",license_plate) VALUES
-	 (2,'Shell Konstanz','2021-09-15 16:12:11',1.579,23.6,0,'',356.1,'KNGH34');
