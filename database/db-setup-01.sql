884265c7506b57b57fecfbb3f61ac60209a069b0
roland-burke <roland.burke@gmx.de>
2021-12-24 14:18:01 +0100

    added trigger and fixed test data

--- database/db-setup-01.sql
+++ database/db-setup-01.sql
@@ -6,6 +6,28 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
+CREATE FUNCTION trigger_insert_logic() 
+RETURNS TRIGGER AS $$
+
+declare
+mileageCount int;
+
+declare
+dateCount int;
+
+BEGIN
+-- trigger logic
+
+SELECT count(*) INTO mileageCount FROM refuel WHERE mileage > NEW.mileage AND users_id = NEW.users_id;
+SELECT count(*) INTO dateCount FROM refuel WHERE date_time > NEW.date_time AND users_id = NEW.users_id;
+
+	IF mileageCount != dateCount THEN
+		RAISE EXCEPTION 'Mileage has already been reached'; 
+	END IF;
+		RETURN NEW;
+END;
+$$ LANGUAGE PLPGSQL;
+
 CREATE TABLE IF NOT EXISTS users (
     users_id                SERIAL NOT NULL PRIMARY key,
     username                varchar(30) NOT NULL,
@@ -17,8 +39,11 @@ CREATE TABLE IF NOT EXISTS refuel (
     id                      SERIAL NOT NULL,
     users_id                int,
     description             varchar(50) NOT NULL,
-    date_time               timestamp NOT NULL constraint realistic_date CHECK ('1960-01-01 0:0:0' < date_time AND date_trunc('day', date_time) <= CURRENT_DATE), 
-    price_per_liter_euro    float8 NOT NULL constraint price_euro_zero_or_positive CHECK (price_per_liter_euro >= 0 AND price_per_liter_euro < 50),
+    date_time               timestamp NOT NULL constraint realistic_date
+	                                                CHECK ('1960-01-01 0:0:0' < date_time
+	                                                       AND date_trunc('day', date_time) <= CURRENT_DATE), 
+    price_per_liter_euro    float8 NOT NULL constraint price_euro_zero_or_positive
+	                                             CHECK (price_per_liter_euro >= 0 AND price_per_liter_euro < 50),
     total_liter             float8 NOT NULL constraint total_liter_positive CHECK (total_liter > 0 AND total_liter < 2000),
     price_per_liter         float8 NOT NULL constraint price_zero_or_positive CHECK (price_per_liter >= 0),
     currency                varchar(12) NOT NULL,
@@ -28,6 +53,11 @@ CREATE TABLE IF NOT EXISTS refuel (
 	PRIMARY KEY (license_plate, mileage),
     constraint fk_users foreign key(users_id) references users(users_id)
 );
+
+CREATE TRIGGER check_on_insert
+BEFORE INSERT OR UPDATE ON refuel
+FOR EACH ROW
+EXECUTE FUNCTION trigger_insert_logic();
 
 CREATE TRIGGER set_timestamp_refuel
 BEFORE UPDATE ON refuel
--- database/dev/db-init-data-dev.sql
+++ database/dev/db-init-data-dev.sql
@@ -15,10 +15,11 @@ INSERT INTO refuel (users_id,"description",date_time,price_per_liter_euro,total_
 
 
 INSERT INTO refuel (users_id,"description",date_time,price_per_liter_euro,total_liter,price_per_liter,"currency","mileage",license_plate) VALUES
-	 (2,'Shell Konstanz','2020-08-13 16:12:11',1.579,23.6,0,'',33620,'KN-GH-34');
+	 (2,'LPG Kreuzlingen Test','2021-09-15 16:12:11',1.368,34.0,1.420,'chf',33620,'KN-QW-123');
 
 INSERT INTO refuel (users_id,"description",date_time,price_per_liter_euro,total_liter,price_per_liter,"currency","mileage",license_plate) VALUES
-	 (2,'LPG Kreuzlingen','2021-09-04 13:10:25',1.439,42.0,1.488,'chf',33000,'KN-KN-9889');
+	 (2,'LPG Kreuzlingen','2021-09-04 13:10:25',1.439,42.0,1.488,'chf',32400,'KN-KN-9889');
 
 INSERT INTO refuel (users_id,"description",date_time,price_per_liter_euro,total_liter,price_per_liter,"currency","mileage",license_plate) VALUES
-	 (2,'LPG Kreuzlingen Test','2021-09-15 16:12:11',1.368,34.0,1.420,'chf',32400,'KN-QW-123');
+	 (2,'Shell Konstanz','2020-08-13 16:12:11',1.579,23.6,0,'',32000,'KN-GH-34');
+
