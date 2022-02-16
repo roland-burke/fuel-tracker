31e62b3c3650983695338acbe4015468f72a59f9
roland-burke <roland.burke@gmx.de>
2022-02-16 23:37:57 +0100

    added one more test entry

--- database/test/init.sql
+++ database/test/init.sql
@@ -8,5 +8,8 @@ INSERT INTO refuel (users_id,"description",date_time,price_per_liter_euro,total_
 	 (1,'description1','2021-09-04 13:10:25',1.439,42.0,1.488,'chf',40100,'KN-KN-9999');
 
 INSERT INTO refuel (users_id,"description",date_time,price_per_liter_euro,total_liter,price_per_liter,"currency","mileage",license_plate) VALUES
-	 (2,'description3','2021-09-05 13:10:25',1.234,50.0,1.345,'chf',50100,'KN-KN-8888');
+	 (1,'description2','2021-09-06 13:10:25',1.439,44.0,1.488,'chf',40800,'KN-KN-9999');
+
+INSERT INTO refuel (users_id,"description",date_time,price_per_liter_euro,total_liter,price_per_liter,"currency","mileage",license_plate) VALUES
+	 (2,'description3','2021-09-08 13:10:25',1.234,51.0,1.345,'chf',41500,'KN-KN-8888');
 
